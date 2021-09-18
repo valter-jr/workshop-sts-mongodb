3e5322040b89315a328ece5f34223cae8bc84c70
Valter Junior <valter.jrads@gmail.com>
2021-09-18 18:56:18 -0300

    Getting a User by Id

--- src/main/java/com/valterjr/workshopmongo/resources/UserResource.java
+++ src/main/java/com/valterjr/workshopmongo/resources/UserResource.java
@@ -5,7 +5,9 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.valterjr.workshopmongo.domain.User;
@@ -24,5 +26,11 @@ public class UserResource {
 		List<User> list = service.findAll();
 		List<UserDTO> DTOlist = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(DTOlist);
+	}
+	
+	@RequestMapping(value = "/{id}", method=RequestMethod.GET)
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(new UserDTO(obj));
 	}
 }
--- src/main/java/com/valterjr/workshopmongo/services/UserService.java
+++ src/main/java/com/valterjr/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.valterjr.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.valterjr.workshopmongo.domain.User;
 import com.valterjr.workshopmongo.repository.UserRepository;
+import com.valterjr.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -22,4 +24,8 @@ public class UserService {
 		userRepository.saveAll(userList);
 	}
 	
+	public User findById(String id) {
+		Optional<User> obj = userRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		}
 }
