6e1e960449429671f8feaa64f2aa54e300fd13b7
Valter Junior <valter.jrads@gmail.com>
2021-09-20 14:33:45 -0300

    Post entity

---
+++ src/main/java/com/valterjr/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.valterjr.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.valterjr.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+
+}
